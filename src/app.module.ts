cab84329ad904797db1ab54b7b8c9920a1aa8cd5
MothWitch <nastoyalove2005lol@gmail.com>
2026-07-01 19:58:31 +0300

    fix: resolve prisma client import

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 import Joi from 'joi';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [
@@ -25,8 +27,9 @@ import Joi from 'joi';
         REFRESH_EXPIRE: Joi.string().default('7d'),
       }),
     }),
+    PrismaModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
